b128d276d370bda30e90f2946704e79b5a31309f
william dutton <will.dutt@gmail.com>
2024-07-29 12:47:51 +1000

    Build deployment pipeline for java

---
+++ .github/bin/versionIncrement.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+cd "$(dirname "$0")"
+
+# Extract the current version from pom.xml
+current_version=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" ./../../pom.xml)
+
+# Remove the -SNAPSHOT suffix to get the release version
+release_version=$(echo $current_version | sed 's/-SNAPSHOT//')
+
+# Set the next development version (increment the minor version)
+IFS='.' read -r -a version_parts <<< "$release_version"
+major=${version_parts[0]}
+minor=${version_parts[1]}
+patch=${version_parts[2]}
+next_patch=$((patch + 1))
+next_development_version="'$major.$minor.$next_patch-SNAPSHOT'"
+next_version="'$major.$minor.$next_patch'"
+
+# Update the YAML file
+./update_text.sh ./../workflows/publish.yml releaseVersion $next_version
+./update_text.sh ./../workflows/publish.yml developmentVersion $next_development_version
+
+
+git add ./../workflows/publish.yml
+git commit -m "Updated publish.yml with release version: $release_version and development version: $next_development_version"
+
+echo "Updated publish.yml with release version: $release_version and development version: $next_development_version and commited"
